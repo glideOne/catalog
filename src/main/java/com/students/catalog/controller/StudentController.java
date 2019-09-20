a7c3a4a03f25496460c947d7750e59cd9b5e7425
ovidiu.zakarias <ovidiu.zakarias@pitechnologies.ro>
2019-09-20 09:31:25 +0300

    Added notes in StudentService.

--- src/main/java/com/students/catalog/controller/StudentController.java
+++ src/main/java/com/students/catalog/controller/StudentController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class StudentResource {
+public class StudentController {
 
     @Autowired
     private StudentRepo studentRepo;
---
+++ src/main/java/com/students/catalog/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.students.catalog.service;
+
+public class StudentService {
+
+    //apelezi repo
+    //transformi Student in StudentDto
+    //din controller apelezi serviciul qwe
+}
